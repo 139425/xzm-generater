6b7715b37b511dd84edfc726469f2b895e10ab3e
xzm_ <3463197630@qq.com>
2025-01-10 13:37:57 +0800

    commit 1

--- code-generator-basic/src/main/java/com/yupi/generator/MainGenerator.java
+++ code-generator-basic/src/main/java/com/yupi/generator/MainGenerator.java
@@ -1,2 +1,33 @@
-package com.yupi.generator;public class MainGenerator {
+package com.yupi.generator;
+
+import com.yupi.model.MainTempleteConfig;
+import freemarker.template.TemplateException;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainGenerator {
+    public static void main(String[] args) throws TemplateException, IOException {
+        MainTempleteConfig mainTemplateConfig = new MainTempleteConfig();
+        mainTemplateConfig.setAuthor("xzm");
+        mainTemplateConfig.setLoop(false);
+        mainTemplateConfig.setOutputText("求和结果：");
+        doGenerator(mainTemplateConfig);
+    }
+
+    public static void doGenerator(Object model) throws TemplateException, IOException {
+
+        String projectPath = System.getProperty("user.dir")+ File.separator + "code-generator-basic";
+        File parentFile = new File(projectPath).getParentFile();
+        String inputPath = new File(parentFile, "code-generator-demo-projects/acm-template").getAbsolutePath();
+        String outputPath = projectPath;
+
+        //生成静态文件
+        StaticGenerator.copyFilesByHutool(inputPath, outputPath);
+
+        //生成动态文件
+        String inputDynamicPath = projectPath + File.separator + "src/main/resources/templates/MainTemplete.java.ftl";
+        String outputDynamicPath = projectPath + File.separator + "acm-template/src/com/yupi/acm/MainTemplete.java";
+        DynamicGenerator.doGenerate(inputDynamicPath, outputDynamicPath, model);
+    }
 }
--- code-generator-basic/src/main/java/com/yupi/model/MainTempleteConfig.java
+++ code-generator-basic/src/main/java/com/yupi/model/MainTempleteConfig.java
@@ -1,2 +1,19 @@
-package com.yupi.model;public class MainTempleteConfig {
+package com.yupi.model;
+
+
+import lombok.Data;
+
+/**
+ * 动态模板配置
+ */
+
+@Data
+public class MainTempleteConfig {
+
+    private String author = "xzm";
+
+    private String outputText = "sum = ";
+
+    private boolean loop = false;
+
 }
